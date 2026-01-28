3448c496500c200be91662e3cc0e2b801b2113f2
abhivadya1 <abhivadya1@gmail.com>
2026-01-29 05:36:15 +0700

    navbar

--- risklens_ai/settings.py
+++ risklens_ai/settings.py
@@ -64,7 +64,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates'],
-        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
